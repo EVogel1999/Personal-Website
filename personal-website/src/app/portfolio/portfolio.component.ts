8926441b7078f4d270c136d3723701d84b562764
EVogel1999 <eav990@gmail.com>
2019-06-16 18:52:43 -0400

    Scaffold portfolio

--- personal-website/src/app/portfolio/portfolio.component.ts
+++ personal-website/src/app/portfolio/portfolio.component.ts
@@ -3,9 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.sass']
+  styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
+
+  // TODO: Don't hardcode filterables
+  filters: string[] = [
+    'Angular',
+    'MongoDB',
+    'Express',
+    'Spring Boot',
+    'Professional Projects',
+    'Passion Projects',
+    'Course Projects'
+  ];
 
   constructor() { }
 
