aa909d66d0fe950bbfc896c31ec762564f85f8f7
EVogel1999 <eav990@gmail.com>
2019-06-16 18:07:55 -0400

    Generate portfolio component

--- personal-website/src/app/app.module.ts
+++ personal-website/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
