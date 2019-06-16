8fe36d1d3eb631f47637e49a61a4c9d1a3ff4a7c
EVogel1999 <eav990@gmail.com>
2019-06-16 18:10:57 -0400

    Add route and navigation in navbar

--- personal-website/src/app/app-routing.module.ts
+++ personal-website/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
+  { path: 'portfolio', component: PortfolioComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', redirectTo: '/home', pathMatch: 'full' }
 ];
