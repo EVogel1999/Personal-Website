64db3aaa8c5df80716aeb54958993fe3038b54ce
Emily Vogel <eav990@gmail.com>
2019-11-25 15:31:55 -0500

    Style contact me page

--- src/app/email/email.component.ts
+++ src/app/email/email.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-email',
   templateUrl: './email.component.html',
-  styleUrls: ['./email.component.sass']
+  styleUrls: ['./email.component.scss']
 })
 export class EmailComponent implements OnInit {
 
